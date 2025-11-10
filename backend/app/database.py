b6c81fe1dd9901b0de5eff79389725ad82358db0
cloudnash <fusionmad47@gmail.com>
2025-11-10 21:41:41 +0530

    Update comment header in database.py

--- backend/app/database.py
+++ backend/app/database.py
@@ -1,4 +1,4 @@
-# COPY THIS: backend/app/database.py
+# backend/app/database.py
 from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
